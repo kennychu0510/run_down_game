537e248c8f1892818715a80c499843b973584170
Kenny Chu <kennychu@asl.com.hk>
2023-01-13 10:49:08 +0800

    implemented moving animation

--- src/Player.ts
+++ src/Player.ts
@@ -1,29 +1,48 @@
 import { CANVAS_WIDTH, CANVAS_HEIGHT } from './constant';
 import { Game } from './Game';
 
-const IMAGE_WIDTH = 153;
-const IMAGE_HEIGHT = 24;
-const FRAME_WIDTH = IMAGE_WIDTH / 8;
-
 export const playerImg = new Image();
 playerImg.src = './src/images/player_sprite.png';
+
+const IMAGE_PATH = './src/images/'
 
 export class Player {
   x = CANVAS_WIDTH / 2;
   y = CANVAS_HEIGHT / 2;
-  image = playerImg;
   ctx: CanvasRenderingContext2D;
   game: Game;
-  state: string;
+  state: 'left' | 'right' | 'stand';
   frame = 0;
+  image
+
   constructor(ctx: CanvasRenderingContext2D, game: Game) {
     this.ctx = ctx;
     this.game = game;
     this.state = 'stand';
+    this.image = {
+      name: 'player_static',
+      frames: 2,
+      current: 1,
+      image: new Image()
+    }
+  }
+
+  renderSprite(state: 'left' | 'right' | 'stand', renderFrame: number) {
+    this.image.image.src = IMAGE_PATH + this.image.name + '_' + this.image.current + '.png'
+    this.ctx.drawImage(this.image.image, this.x, this.y)
+
+    if (renderFrame % 5 === 0) {
+      this.image.current++
+      if (this.image.current > this.image.frames) {
+        this.image.current = 1
+      }
+    }
   }
 
   draw(frame: number) {
-    this.ctx.drawImage(this.image, this.frame * FRAME_WIDTH, 0, FRAME_WIDTH, IMAGE_HEIGHT, this.x, this.y, FRAME_WIDTH, IMAGE_HEIGHT);
+    // this.ctx.drawImage(this.image, this.frame * FRAME_WIDTH, 0, FRAME_WIDTH, IMAGE_HEIGHT, this.x, this.y, FRAME_WIDTH, IMAGE_HEIGHT);
+    this.renderSprite('stand', frame)
+
     if (this.y > CANVAS_HEIGHT) {
       this.y = 0;
     }
@@ -38,7 +57,6 @@ export class Player {
       if (this.frame > 3) {
         this.frame = 1;
       }
-      console.log(this.frame);
     } else if (this.state === 'right') {
       this.x++;
     } else {
@@ -49,9 +67,8 @@ export class Player {
   private checkForCollision() {
     const floors = this.game.floors;
     for (let i = 0; i < this.game.floors.length; i++) {
-      if (this.y + IMAGE_HEIGHT >= floors[i].y && this.x <= floors[i].x + floors[i].width && this.x + FRAME_WIDTH > floors[i].x && this.y < floors[i].y) {
-        this.y = floors[i].y - IMAGE_HEIGHT;
-        // this.frame = 0;
+      if (this.y + this.image.image.height >= floors[i].y && this.x <= floors[i].x + floors[i].width && this.x + this.image.image.width > floors[i].x && this.y < floors[i].y) {
+        this.y = floors[i].y - this.image.image.height;
         return true;
       }
     }
